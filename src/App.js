65cb971977434c58ac9ab3b349e2cb72ceb895b9
DS436 <102336057+DS436@users.noreply.github.com>
2024-05-31 14:18:45 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import Faq from './components/Faq';
+import ContactForm from './components/ContactForm';
+
+const App = () => (
+    <div className="App">
+        <Hero />
+        <Features />
+        <Faq />
+        <ContactForm />
+    </div>
+);
+
+export default App;
